fd23d479651e84df1b6f3dd62dd80a0fe0675051
Nikhil Chandna <nikhil.chandna@zopsmart.com>
2023-10-16 18:04:58 +0530

    fix image product issue

--- components/Product/index.jsx
+++ components/Product/index.jsx
@@ -10,7 +10,14 @@ const Product = ({ product, onDelete }) => {
     <div className="flex flex-col border-2 w-90 h-90 justify-center items-center p-8 rounded-lg bg-white ">
       <Link href={`/info/${product.id}`}>
         <div className="h-48">
-          <img src={product.image} width={120} height={200} />
+          <img
+            onClick={() => {
+              handleProduct(product);
+            }}
+            src={product.image}
+            width={120}
+            height={200}
+          />
         </div>
       </Link>
       <div className="flex flex-col justify-center items-end p-4 gap-4 w-full h-full">
